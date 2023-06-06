36ef469ee3a6524b0913341ad293d90bcd6710f4
NMPotato <ji.figueroa.lopez@gmail.com>
2023-06-06 18:05:36 -0400

    otravez
    -admin acceso tablas
    -noticia img medio dinamica(solo funcioa con 1(una))

--- Semestral/noticias/admin.py
+++ Semestral/noticias/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Noticia)
+admin.site.register(Tipo_Parti)
+admin.site.register(Parti)
+admin.site.register(Categoria)
